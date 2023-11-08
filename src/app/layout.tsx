db3098244c853bdcaba5c954936c4dde4405c8a3
JhonatanGAlves <jhonatangalves96@gmail.com>
2023-11-07 23:51:45 -0300

    style: 🎨 Defining colors and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,18 @@ import type { Metadata } from "next";
 import { Baloo_2, Roboto } from "next/font/google";
 import "./globals.css";
 
-const balooTwo = Baloo_2({ subsets: ["latin"], weight: ["700", "800"] });
-const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
+const balooTwo = Baloo_2({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["700", "800"],
+  variable: "--font-baloo-two",
+});
+const roboto = Roboto({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "700"],
+  variable: "--font-roboto",
+});
 
 export const metadata: Metadata = {
   title: "Coffee Delivery",
@@ -17,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${balooTwo.className} ${roboto.className}`}>
+      <body className={`${balooTwo.variable} ${roboto.variable}`}>
         {children}
       </body>
     </html>
